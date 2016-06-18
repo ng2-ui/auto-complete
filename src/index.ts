fa4b0c2f7944b1cf09bf3097fbbbf229af25587a
Allen Kim <allenhwkim@gmail.com>
2016-06-18 16:25:00 -0400

    updated index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,3 @@
-"use strict";
-
-function __export(m) {
-  for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
-}
-__export(require('./auto-complete'));
-__export(require('./auto-complete.component'));
-__export(require('./auto-complete.directive'));
+export { AutoComplete } from './auto-complete';
+export { AutoCompleteComponent } from './auto-complete.component';
+export { AutoCompleteDirective } from './auto-complete.directive';

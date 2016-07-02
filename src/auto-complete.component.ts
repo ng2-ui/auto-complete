4f4b128e64b1c1e1eea7f2b412227b89838b80f7
Allen Kim <allenhwkim@gmail.com>
2016-07-02 13:19:48 -0400

    issue #2

--- src/auto-complete.component.ts
+++ src/auto-complete.component.ts
@@ -152,7 +152,7 @@ export class AutoCompleteComponent implements OnInit {
 
   private delay = (function(){
     var timer = 0;
-    return function(callback: any, ms){
+    return function(callback: any, ms: number){
       clearTimeout(timer);
       timer = setTimeout(callback, ms);
     };

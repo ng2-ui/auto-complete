0da30b758e218119f0daa46444663cbf59da2948
Manuel Pacheco <manuelalejandropm@gmail.com>
2016-12-05 08:24:31 -0400

    initial support for aot:
    - update npm dependencies
    - split exported module in its own file
    - setup ngc
    - setup aot webpack plugin.

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 var path = require('path');
 var webpack = require('webpack');
+var AotPlugin = require('@ngtools/webpack').AotPlugin;
 
 console.log(path.join(__dirname, 'src', 'index.ts'));
 
@@ -8,7 +9,7 @@ module.exports = {
     'ng2-auto-complete': path.join(__dirname, 'src', 'index.ts')
   },
   resolve: {
-    extensions: ['', '.ts', '.js', '.json', '.css', '.html']
+    extensions: ['.ts', '.js', '.json', '.css', '.html']
   },
   output: {
     path: path.join(__dirname, 'dist'),
@@ -25,8 +26,14 @@ module.exports = {
     loaders: [
       { // Support for .ts files.
         test: /\.ts$/,
-        loaders: ['ts', 'angular2-template-loader']
+        loaders: ['@ngtools/webpack', 'angular2-template-loader']
       }
     ]
-  }
+  },
+  plugins: [
+    new AotPlugin({
+      tsConfigPath: path.resolve(__dirname, 'tsconfig.json'),
+      entryModule: path.resolve(__dirname, 'src', 'ng2-auto-complete.module') + '#Ng2AutoCompleteModule'
+    })
+  ]
 };

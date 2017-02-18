e725cf00b56121405c483f8a05ce3d9de6d68460
ncunningham <ndcunningham@gmail.com>
2017-02-17 20:51:43 -0500

    (feat) - Webpack  Upgrade Moving from Version 1 to 2
    add vscode to gitignore

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,4 @@
-var path = require('path');
+var path = require("path");
 var webpack = require('webpack');
 
 var config = {
@@ -6,10 +6,10 @@ var config = {
     'ng2-auto-complete': path.join(__dirname, 'src', 'index.ts')
   },
   resolve: {
-    extensions: ['', '.ts', '.js', '.json', '.css', '.html']
+    extensions: ['.ts', '.js', '.json', '.css', '.html']
   },
   resolveLoader: {
-    root: path.join(__dirname, 'node_modules')
+    modules: [path.join(__dirname, 'node_modules')]
   },
   output: {
     path: path.join(__dirname, 'dist'),
@@ -23,19 +23,20 @@ var config = {
   ],
   devtool: 'source-map',
   module: {
-    loaders: [
+    rules: [
       { // Support for .ts files.
         test: /\.ts$/,
-        loaders: ['ts', 'angular2-template-loader']
+        use: ['ts-loader', 'angular2-template-loader']
       }
     ]
   }
 };
 
 //Different Environment Setup
+
 if (process.env.NODE_ENV === 'prod') {
-  config.module.loaders.push({
-    test: /\.ts$/, loader: 'strip-loader?strip[]=debug,strip[]=console.log'
+  config.module.rules.push({
+    test: /\.ts$/, use: 'strip-loader?strip[]=debug,strip[]=console.log'
   });
 }
 

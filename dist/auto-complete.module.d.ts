b3638f35ed0c009fb6c39108f9c78aa644b2930c
Igor Artasevych <igor.artasevych@amadeus.com>
2017-09-28 18:19:44 +0300

    Added a blur-after-click option to customize IE workaround (Blur event)

--- dist/auto-complete.module.d.ts
+++
@@ -1,7 +0,0 @@
-import { NguiAutoComplete } from './auto-complete';
-export declare class NguiAutoCompleteModule {
-    static forRoot(): {
-        ngModule: typeof NguiAutoCompleteModule;
-        providers: typeof NguiAutoComplete[];
-    };
-}

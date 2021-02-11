f5c003eefd88d643d40199ecd651efda226852b7
omostepaniuk <o.mostepaniuk@gmail.com>
2021-02-11 17:42:05 +0200

    Porting SP auto-complete custom changes onto upstream auto-complete v3.0.0:
     - export AutoCompleteFilter and NguiAutoCompleteNoMatchFoundMessage via public-api.ts

---
+++ projects/auto-complete/src/lib/model/index.ts
@@ -0,0 +1,2 @@
+export { AutoCompleteFilter } from './auto-complete.filter';
+export { NguiAutoCompleteNoMatchFoundMessage } from './no-match-found-message.model';
--- projects/auto-complete/src/public-api.ts
+++ projects/auto-complete/src/public-api.ts
@@ -6,3 +6,4 @@ export * from './lib/auto-complete.service';
 export * from './lib/auto-complete.component';
 export * from './lib/auto-complete.directive';
 export * from './lib/auto-complete.module';
+export * from './lib/model'
